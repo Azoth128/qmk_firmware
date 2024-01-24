b6139dcf17da1964e07019630e7c4630005c5193
Tim Wundenberg <tim.wundenberg@breuninger.de>
2024-01-24 17:47:11 +0100

    make + = and - more accessible and add ,. numbers

--- keyboards/beekeeb/piantor/keymaps/azoth128/keymap.c
+++ keyboards/beekeeb/piantor/keymaps/azoth128/keymap.c
@@ -10,11 +10,11 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │   │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ Ü │
+      * │ + │ Q │ W │ E │ R │ T │       │ Y │ U │ I │ O │ P │ Ü │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │SFT│ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ Ö │ Ä │
+      * │ = │ A │ S │ D │ F │ G │       │ H │ J │ K │ L │ Ö │ Ä │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │   │
+      * │ - │ Z │ X │ C │ V │ B │       │ N │ M │ , │ . │ / │ ß │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │ESC├───┐           ┌───┤DEL│
@@ -24,12 +24,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                                SYM
       */
     [_BASE] = LAYOUT_split_3x6_3(
-        KC_NO,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   DE_UDIA,
+        KC_KP_PLUS,  KC_Q,   KC_W,   KC_E,   KC_R,   KC_T,       KC_Y,   KC_U,   KC_I,   KC_O,   KC_P,   DE_UDIA,
 
-        KC_LSFT,    MT(MOD_LGUI,KC_A),  MT(MOD_LCTL,KC_S),   MT(MOD_LALT,KC_D),   MT(MOD_LSFT,KC_F),    KC_G,
+        KC_KP_EQUAL, MT(MOD_LGUI,KC_A),  MT(MOD_LCTL,KC_S),   MT(MOD_LALT,KC_D),   MT(MOD_LSFT,KC_F),    KC_G,
         KC_H,       MT(MOD_RSFT,KC_J),  MT(MOD_RALT,KC_K),   MT(MOD_RCTL,KC_L),   MT(MOD_RGUI,DE_ODIA), DE_ADIA,
 
-        KC_NO,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   DE_COMM,DE_DOT, DE_SLSH,    DE_SS,
+        KC_KP_MINUS,  KC_Z,   KC_X,   KC_C,   KC_V,   KC_B,       KC_N,   KC_M,   DE_COMM,DE_DOT, DE_SLSH,    DE_SS,
 
         KC_ESC,                 LT(_NAVIGATION,KC_SPC), KC_TAB,
         LT(_SYMBOLS,KC_ENT),    LT(_NUMBERS,KC_BSPC),   LT(_FUNCTIONS,KC_DELETE)
@@ -37,11 +37,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │   │ - │ 7 │ 5 │ 9 │ * │       │   │   │   │   │   │   │
+      * │ + │   │ 7 │ 5 │ 9 │ * │       │   │   │   │   │   │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ = │ 4 │ 5 │ 6 │ / │       │   │   │   │   │   │   │
+      * │ = │   │ 4 │ 5 │ 6 │   │       │   │SFT│ALT│CTL│GUI│   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │   │ 1 │ 2 │ 3 │ + │       │   │   │   │   │   │   │
+      * │ - │   │ 1 │ 2 │ 3 │   │       │   │   │ , │ . │ / │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │ESC├───┐           ┌───┤DEL│
@@ -50,22 +50,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [_NUMBERS] = LAYOUT_split_3x6_3(
-        KC_NO,  KC_KP_MINUS,KC_7,    KC_8,    KC_9,    KC_KP_ASTERISK,     KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-
-        KC_NO,  KC_KP_EQUAL,        KC_4,            KC_5,            KC_6,            KC_KP_SLASH,
-        KC_NO,  MT(MOD_RSFT,KC_NO), MT(MOD_RALT,KC_NO), MT(MOD_RCTL,KC_NO), MT(MOD_RGUI,KC_NO), KC_NO,
-
-        KC_NO,  KC_NO,      KC_1,    KC_2,    KC_3,    KC_KP_PLUS,         KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
-                                        KC_ESC,     KC_0,    KC_TAB,             KC_ENT, KC_NO,  KC_DELETE
+        KC_KP_PLUS, KC_NO,KC_7,    KC_8,    KC_9,    KC_KP_ASTERISK,    KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,  KC_NO,
+        KC_KP_EQUAL,KC_NO,KC_4,    KC_5,    KC_6,    KC_NO,             KC_NO,  KC_RSFT, KC_RALT, KC_RCTL, KC_RGUI, KC_NO,
+        KC_KP_MINUS,KC_NO,KC_1,    KC_2,    KC_3,    KC_NO,             KC_NO,  KC_NO,  DE_COMM,DE_DOT, DE_SLSH,    KC_NO,
+        KC_ESC,     KC_0,    KC_TAB,             KC_ENT, KC_NO,  KC_DELETE
     ),
 
      /*
       * ┌───┬───┬───┬───┬───┬───┐       ┌───┬───┬───┬───┬───┬───┐
-      * │   │ ! │ § │ { │ } │ % │       │ ` │ & │ ? │ \ │ ° │   │
+      * │ + │ ! │ § │ { │ } │ % │       │ ` │ & │ ? │ \ │ ° │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ # │ $ │ ( │ ) │ ~ │       │ ' │ " │ ; │ | │ _ │   │
+      * │ = │ # │ $ │ ( │ ) │ ~ │       │ ' │ " │ ; │ | │ _ │   │
       * ├───┼───┼───┼───┼───┼───┤       ├───┼───┼───┼───┼───┼───┤
-      * │   │ < │ > │ [ │ ] │ € │       │ ^ │ @ │ : │ / │   │   │
+      * │ - │ < │ > │ [ │ ] │ € │       │ ^ │ @ │ : │ / │   │   │
       * └───┴───┴───┴───┴───┴───┘       └───┴───┴───┴───┴───┴───┘
       *               ┌───┐                   ┌───┐
       *               │ESC├───┐           ┌───┤DEL│
@@ -74,9 +71,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
       *                       └───┘   └───┘
       */
     [_SYMBOLS] = LAYOUT_split_3x6_3(
-        KC_NO,  DE_EXLM,DE_SECT,DE_LCBR,DE_RCBR,DE_PERC,        DE_GRV, DE_AMPR,DE_QUES,DE_BSLS,DE_DEG, KC_NO,
-       KC_NO,  DE_HASH,DE_DLR, DE_LPRN,DE_RPRN,DE_TILD,        DE_QUOT,DE_DQUO,DE_SCLN,DE_PIPE,DE_UNDS,KC_NO,
-        KC_NO,  DE_LABK,DE_RABK,DE_LBRC,DE_RBRC,DE_EURO,        DE_CIRC,DE_AT,  DE_COLN,DE_SLSH,KC_NO,  KC_NO,
+        KC_KP_PLUS,  DE_EXLM,DE_SECT,DE_LCBR,DE_RCBR,DE_PERC,        DE_GRV, DE_AMPR,DE_QUES,DE_BSLS,DE_DEG, KC_NO,
+        KC_KP_EQUAL, DE_HASH,DE_DLR, DE_LPRN,DE_RPRN,DE_TILD,        DE_QUOT,DE_DQUO,DE_SCLN,DE_PIPE,DE_UNDS,KC_NO,
+        KC_KP_MINUS, DE_LABK,DE_RABK,DE_LBRC,DE_RBRC,DE_EURO,        DE_CIRC,DE_AT,  DE_COLN,DE_SLSH,KC_NO,  KC_NO,
                                 KC_ESC, KC_BSPC,KC_TAB,         KC_NO,  KC_BSPC,KC_DELETE
     ),
 
